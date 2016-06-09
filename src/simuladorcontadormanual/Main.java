5dd0130bb3895ce1417df00e64db583671cd7ba5
Carlos Casanova Ortegón <14470413@itcampeche.edu.mx>
2016-06-08 22:43:47 -0500

    Simulador de contador manual

---
+++ src/simuladorcontadormanual/Main.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuladorcontadormanual;
+
+import Gui.MainForm;
+import contadormanual.ContadorManual;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainForm windows =new MainForm();
+        windows.setTitle("Simulador de Contador Manual ");
+        windows.setVisible(true);
+        windows.setLocationRelativeTo(null);
+        
+//        ContadorManual contadorDePasajeros = new ContadorManual();
+//        
+//        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
+//        contadorDePasajeros.contar();
+//        System.out.println("Click");
+//        contadorDePasajeros.contar();
+//        System.out.println("Click");
+//        contadorDePasajeros.contar();
+//        System.out.println("Click");
+//        contadorDePasajeros.contar();
+//        System.out.println("Click");
+//        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
+//        System.out.println("Se reiniciará el conteo de pasajeros...");
+//        contadorDePasajeros.reiniciarConteo();
+//        System.out.println("Número de pasajeros: " + contadorDePasajeros.getConteoActual());
+    }
+}
